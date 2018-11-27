1c73ed84b615a5bd9fcf8b34d18d42046b067bf8
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-11-26 20:44:50 -0500

    set up sentry!

--- learn-redux/client/data/config.js
+++ learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = '09d93d04d1464364a4f37ada41799fc3';
+const sentry_app = '1331156';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -14,6 +14,13 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
+import Raven from 'raven-js';
+import { sentry_url } from './data/config';
+
+Raven.config(sentry_url).install();
+
+console.log('window'.iser());
+
 const Reduxstagram = () => (
   <Provider store={store}>
     <Router history={history}>
