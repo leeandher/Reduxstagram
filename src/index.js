3cc20443fc8f6a7441523fd4722aa446fcb24ab6
Leander Rodrigues <me@leander.xyz>
2019-04-14 20:57:19 -0400

    🔖 Create v2.0.0 based on CRA instead of standalone webpack

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-//Import CSS
-import css from './styles/style.styl';
+import React from 'react'
+import { render } from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
 //Import Components
-import App from './components/App';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
+import App from './components/App'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
 //Import React Router Dependencies
-import { Router, Route, IndexRoute } from 'react-router';
-import { Provider } from 'react-redux';
-import store, { history } from './store';
+import { Router, Route, IndexRoute } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
 
 /* CRASH REPORTING */
 
 //Import our SDK
-import Raven from 'raven-js';
+import Raven from 'raven-js'
+
 //Import our error stream (and a custom exception handling method)
-import { sentry_url, logException } from './data/config';
+import { sentry_url } from './data/config'
 
 //Let Sentry watch our code for errors
-Raven.config(sentry_url).install();
+Raven.config(sentry_url).install()
 
 //Send custom data along with all errors
 // Raven.config(sentry_url, {
@@ -52,6 +52,8 @@ const Reduxstagram = () => (
       </Route>
     </Router>
   </Provider>
-);
+)
 
-render(<Reduxstagram />, document.getElementById('root'));
+render(<Reduxstagram />, document.getElementById('root'))
+
+serviceWorker.unregister()
